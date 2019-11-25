cc6f09e745ed5539b27d3804c48cd409bf777471
Astonnn <last466453@gmail.com>
2019-11-25 14:49:50 +0800

    update

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
+//swap(int,int)
+#define Swap(a,b) do{a^=b;b^=a;a^=b;}while(0)
 //系统参数：数组元素个数、一行打印元素个数、每个元素的打印宽度等
 int num_of_arr,num_in_row,width_of_each;
 //打印数组~
@@ -68,7 +71,7 @@ float average(int a[],int n){
 	return (float)s/n;
 }
 void OrderInsert(int a[], int n, int val);//在有序数组中插入新元素保持有序性
-//冒泡排序
+//冒泡排序~
 void BubbleSort(int a[], int n){
 	int i=0,j;
 	for(;i<n-1;i++){
@@ -79,10 +82,27 @@ void BubbleSort(int a[], int n){
 		}
 	}
 }
-void SelectSort(int a[],int n);//选择排序
-void ExchangeSort(int a[],int n);//交换排序
-int Search(int a[], int n, int val);//普通查找
-//二分查找
+//选择排序~
+void SelectSort(int a[],int n){
+	int i=0,j;
+	for(;i<n-1;i++){
+		int min=i;
+		for(j=i+1;j<n;j++){
+			if(a[j]<a[min])min=i;
+		}
+		int t=a[i];
+		a[i]=a[min];
+		a[min]=t;
+	}
+}
+//交换排序(快速排序?)
+void ExchangeSort(int a[],int n){}
+//普通查找~
+int Search(int a[], int n, int val){
+	while(--n+1)if(a[n]==val)return n;
+	return -1;
+}
+//二分查找~
 int BiSearch(int a[], int n, int val){
 	int l=0,mid,r=n-1;
 	while(r>=l){
@@ -97,12 +117,16 @@ int BiSearch(int a[], int n, int val){
 端。 例如 int b[10]={0,1,2,3,4,5, 6,7,8,9}; 调用 LeftRotate(b,10,4)之后，b的元素为
 {4，5，6，7，8，9，0，1，2，3} */
 int LeftRotate(int a[], int n, int m){
-	m%=n;
+	m=(m>0)?(m%n):(m%n+n);
 //	if(m<0)return RightRotate(a,n,-m);
+	if(m==0)return m;
 	int i=0,t[m];
 	for(;i<m;i++)t[i]=a[i];
 	for(;i<n;i++)a[i-m]=a[i];
 	for(;i>=n-m;--i)a[i]=t[i-n+m];
-	return 1;
+	return m;
 }
-int RightRotate(int a[], int n, int m); //右旋m位
+//右旋m位
+int RightRotate(int a[], int n, int m){
+	return LeftRotate(a,n,-m);
+}
